1c51e6728b83b748ff1f58ab4d07a7ad797e0633
HunterLM <hunter109@gmail.com>
2022-05-15 10:44:23 -0500

    finished associations

--- models/index.js
+++ models/index.js
@@ -4,14 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Associations 
+Product.belongsTo(Category);
 
-// Categories have many Products
+Category.hasMany(Product);
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
+// exports
 module.exports = {
   Product,
   Category,
